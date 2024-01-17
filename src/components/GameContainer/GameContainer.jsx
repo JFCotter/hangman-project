27b084bf30c819df394f1fe06eabaffffd07e72c
BMAgeas <Ben.Mullan@Ageas.Co.UK>
2024-01-17 10:29:47 +0000

    letter-buttons-rendering

--- src/components/GameContainer/GameContainer.jsx
+++ src/components/GameContainer/GameContainer.jsx
@@ -3,17 +3,29 @@ import LetterOutput from "../../components/LetterOutput/LetterOutput.jsx";
 import LetterButton from "../../components/LetterButton/LetterButton.jsx";
 
 const GameContainer = () => {
+
   // Using a Set, so that by-design, the same letter cannot be added twice.
   const [guessedLetters, setGuessedLetters] = React.useState(new Set([]));
 
+  // ["A", "B", "C", ...]
+  const alphabet = new Set(Array.from({length : 26}, (e, i) => i + 65).map(e => String.fromCharCode(e)));
+
   return (
     <div>
-      <b>Hangman</b>
-      <br />
 
-      {[..."Test"].map((letter, index) => (
-        <LetterOutput key={index} />
-      ))}
+		<b>Hangman</b>
+		<br />
+
+		{[..."Test"].map((letter, index) => (
+		<LetterOutput key={index} />
+		))}
+
+		<br/><br/>
+
+		{
+			[...alphabet].map(letter => <LetterButton key={letter} letterOfAlphabet={letter} />)
+		}
+
     </div>
   );
 };
