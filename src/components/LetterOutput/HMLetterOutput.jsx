cf4d5525720cfa1e7651747f48913044f9431335
Cian Perrett <Cian.Perrett@ageas.co.uk>
2024-01-17 09:49:07 +0000

    ammending component names

---
+++ src/components/LetterOutput/HMLetterOutput.jsx
@@ -0,0 +1,12 @@
+import React	from "react"
+import styles	from "./HMLetterOutput.module.css"
+
+const HMLetterOutput = () => {
+	return (
+		<span className={styles.HMLetterOutput}>
+			_
+		</span>
+	)
+}
+
+export default HMLetterOutput
---
+++ src/components/Status/HMStatus.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const HMStatus = () => {
+	return (
+		<figure>
+			<img src="/HM_Stage_11.PNG" alt="Hangman Graphic" />
+			<figcaption>11 guess(es) remaining</figcaption>
+		</figure>
+	)
+}
+
+export default HMStatus
