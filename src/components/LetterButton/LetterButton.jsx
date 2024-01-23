e64c215bc644b29c6616163416486ade122d0ff0
Cian Perrett <Cian.Perrett@ageas.co.uk>
2024-01-23 15:44:02 +0000

    disabling-buttons

--- src/components/LetterButton/LetterButton.jsx
+++ src/components/LetterButton/LetterButton.jsx
@@ -2,9 +2,13 @@ import React	from "react";
 import styles	from "./LetterButton.module.scss"
 
 const LetterButton = ({ letterOfAlphabet, guessedLettersP, setGuessedLettersP }) => {
+
+	const hasBeenGuessed = [...guessedLettersP].includes(letterOfAlphabet.toUpperCase())
+
 	return (
 		<button
-			className={styles.LetterButton}
+			className={`${styles.LetterButton} ${hasBeenGuessed && styles.hasBeenGuessed}`}
+			disabled={hasBeenGuessed}
 			onClick = {
 				() => setGuessedLettersP(new Set([...guessedLettersP, letterOfAlphabet.toUpperCase()]))
 			}
