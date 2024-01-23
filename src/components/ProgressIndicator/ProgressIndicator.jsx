2f83e86b4bfa5215e4d2082f012859651b1a76bd
JFCotter <james.cotter@ageas.co.uk>
2024-01-23 11:42:57 +0000

    Added scss for ProgressIndicator

--- src/components/ProgressIndicator/ProgressIndicator.jsx
+++ src/components/ProgressIndicator/ProgressIndicator.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import styles from "./ProgressIndicator.module.scss";
 
 const ProgressIndicator = () => {
   return (
     <figure>
-      <img src="/stages/stage_11.PNG" alt="Hangman Graphic" />
+      <img src="/stages/stage_11.PNG" className={styles.ProgressIndicator} alt="Hangman Graphic" />
       <figcaption>11 guess(es) remaining</figcaption>
     </figure>
   );
