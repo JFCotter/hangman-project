0793a28b42f2060868a770835bcb9b3f6fcc1450
BMAgeas <Ben.Mullan@Ageas.Co.UK>
2024-01-23 15:19:53 +0000

    Spin-to-reveal

--- src/components/GameContainer/GameContainer.jsx
+++ src/components/GameContainer/GameContainer.jsx
@@ -6,43 +6,40 @@ import ProgressIndicator from "../ProgressIndicator/ProgressIndicator.jsx";
 
 const GameContainer = () => {
 	
-	// setGameWord not needed because the gameWord should only be set on page load
+	// setGameWord not needed because the gameWord should only be set on page load.
 	const [gameWord] = React.useState( getRandomWord().toUpperCase() );
+	console.debug(`gameWord: ${gameWord}`);
 
 	// Using a Set, so that by-design, the same letter cannot be added twice.
 	const [guessedLetters, setGuessedLetters] = React.useState(new Set([]));
 
 	// ["A", "B", "C", ...]
-	const alphabet = new Set(
-		Array.from({ length : 26 }, (e, i) => i + 65).map((e) =>
-			String.fromCharCode(e)
-		)
+	const alphabet = new Set(Array.from({ length : 26 }, (e, i) => i + 65).map(e => String.fromCharCode(e)));
+
+	return (
+		<div>
+			
+			<ProgressIndicator guessedLettersP={guessedLetters} gameWordP={gameWord} />
+
+			<br />
+
+			{
+				[...gameWord].map(
+					(letter, index) => <LetterOutput key={index} targetLetter={letter.toUpperCase()} guessedLettersP={guessedLetters} />
+				)
+			}
+
+			<br />
+
+			{
+				[...alphabet].map(
+					(letter, index) => <LetterButton key={index} letterOfAlphabet={letter.toUpperCase()} guessedLettersP={guessedLetters} setGuessedLettersP={setGuessedLetters} />
+				)
+			}
+
+		</div>
 	);
 
-  return (
-	<div>
-		
-		<ProgressIndicator guessedLettersP={guessedLetters} gameWordP={gameWord} />
-
-		<br />
-
-		{
-			[...gameWord].map(
-				(letter, index) => <LetterOutput key={index} targetLetter={letter.toUpperCase()} guessedLettersP={guessedLetters} />
-			)
-		}
-
-		<br />
-		<br />
-
-		{
-			[...alphabet].map(
-				(letter, index) => <LetterButton key={index} letterOfAlphabet={letter.toUpperCase()} guessedLettersP={guessedLetters} setGuessedLettersP={setGuessedLetters} />
-			)
-		}
-
-	</div>
-  );
 };
 
 export default GameContainer;
--- src/components/LetterOutput/LetterOutput.jsx
+++ src/components/LetterOutput/LetterOutput.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import styles from "./LetterOutput.module.scss";
 
 const LetterOutput = ({ targetLetter, guessedLettersP }) => {
+
+	const hasBeenGuessed = [...guessedLettersP].includes(targetLetter.toUpperCase());
+
 	return (
-		<span className={styles.LetterOutput}>
-			{
-				[...guessedLettersP].includes(targetLetter.toUpperCase())
-					? targetLetter.toUpperCase()
-					: "_"
-			}
+		<span className={`${styles.letterOutput} ${hasBeenGuessed && styles.hasBeenGuessed}`}>
+			{ hasBeenGuessed ? targetLetter.toUpperCase() : "_" }
 		</span>
 	);
+
 };
 
 export default LetterOutput;
