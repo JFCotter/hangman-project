e3490e794ab08d5a5dd36c43082c442767560f12
JFCotter <james.cotter@ageas.co.uk>
2024-01-23 11:46:39 +0000

    renamed class to .hangmanGraphic

--- src/components/ProgressIndicator/ProgressIndicator.jsx
+++ src/components/ProgressIndicator/ProgressIndicator.jsx
@@ -4,7 +4,7 @@ import styles from "./ProgressIndicator.module.scss";
 const ProgressIndicator = () => {
   return (
     <figure>
-      <img src="/stages/stage_11.PNG" className={styles.ProgressIndicator} alt="Hangman Graphic" />
+      <img src="/stages/stage_11.PNG" className={styles.hangmanGraphic} alt="Hangman Graphic" />
       <figcaption>11 guess(es) remaining</figcaption>
     </figure>
   );
