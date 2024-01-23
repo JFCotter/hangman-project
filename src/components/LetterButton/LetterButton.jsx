3ce683681b297e98dafffadc1ef3effb2c1777cb
Cian Perrett <Cian.Perrett@ageas.co.uk>
2024-01-23 10:50:28 +0000

    changing-global-to-lowercase

--- src/components/LetterButton/LetterButton.jsx
+++ src/components/LetterButton/LetterButton.jsx
@@ -1,5 +1,5 @@
 import React	from "react";
-import styles	from "./LetterButton.module.css";
+import styles	from "./LetterButton.module.scss"
 
 const LetterButton = ({ letterOfAlphabet, guessedLettersP, setGuessedLettersP }) => {
 	return (
