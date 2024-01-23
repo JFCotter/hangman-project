487abb1e7345486ad9c786427d85f7ebd5090863
BMAgeas <Ben.Mullan@Ageas.Co.UK>
2024-01-23 10:51:07 +0000

    Removed LetterButton debugging lines

--- src/components/LetterOutput/LetterOutput.jsx
+++ src/components/LetterOutput/LetterOutput.jsx
@@ -5,12 +5,9 @@ const LetterOutput = ({ targetLetter, guessedLettersP }) => {
 	return (
 		<span className={styles.LetterOutput}>
 			{
-				(
-					() => {
-						console.info(`Rendering LetterOutput for ${targetLetter.toUpperCase()} as ${[...guessedLettersP].includes(targetLetter.toUpperCase()) ? targetLetter.toUpperCase() : "_"}\r\nguessedLettersP: ${[...guessedLettersP]} includes ${targetLetter.toUpperCase()} = ${[...guessedLettersP].includes(targetLetter.toUpperCase())}`);
-						return ([...guessedLettersP].includes(targetLetter.toUpperCase()) ? targetLetter.toUpperCase() : "_");
-					}
-				)()
+				[...guessedLettersP].includes(targetLetter.toUpperCase())
+					? targetLetter.toUpperCase()
+					: "_"
 			}
 		</span>
 	);
