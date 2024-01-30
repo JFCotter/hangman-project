3793204fff381dcd2180f09dfee6116c64f955a1
BMAgeas <Ben.Mullan@Ageas.Co.UK>
2024-01-30 14:04:01 +0000

    React Router working for multiple pages (inc. `/game`)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-import React from "react";
-import Header from "./components/Header/Header.jsx";
-import Footer from "./components/Footer/Footer.jsx";
-import GameContainer from "./components/GameContainer/GameContainer.jsx";
+import React							from "react";
+import {BrowserRouter, Routes, Route}	from "react-router-dom";
+import Header							from "./components/Header/Header.jsx";
+import Footer							from "./components/Footer/Footer.jsx";
+import GameContainer					from "./components/GameContainer/GameContainer.jsx";
+import LandingPageContent				from "./components/LandingPageContent/LandingPageContent.jsx";
 import "./globals.scss";
 
 const App = () => {
-  return (
-    <>
-      <Header />
-      <main>
-        <GameContainer />
-      </main>
-      <Footer />
-    </>
-  );
+
+	// (To be changed!)
+	document.body.style.backgroundImage = `url("/DesertBackdrop.png")`;
+	//document.body.style.filter = "blur(3px)";
+
+	return (
+		<>
+			<BrowserRouter>
+				<Header/>
+				<main>
+					<Routes>
+						<Route path="/"		element={<LandingPageContent/>}	/>
+						<Route path="/game" element={<GameContainer/>}		/>
+					</Routes>
+				</main>
+				<Footer/>
+			</BrowserRouter>
+		</>
+	);
+
 };
 
 export default App;
