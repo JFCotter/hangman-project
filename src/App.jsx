2ac467c377d17b5ea9137b4ecdef9f792bb05e2c
BMAgeas <Ben.Mullan@Ageas.Co.UK>
2024-01-30 11:29:39 +0000

    Added `variables.scss` and `$siteFont`

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import GameContainer from "./components/GameContainer/GameContainer.jsx";
-import "./globals.css";
+import "./globals.scss";
 
 const App = () => {
   return (
