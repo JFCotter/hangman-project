ec3f3c7ac2b85032f26213672323bc02d5678988
JFCotter <james.cotter@ageas.co.uk>
2024-02-07 09:35:55 +0000

    moved ul elements out of p elements

--- src/components/LandingPageContent/LandingPageContent.jsx
+++ src/components/LandingPageContent/LandingPageContent.jsx
@@ -16,28 +16,26 @@ const LandingPageContent = () => {
 					<br/>
 					<br/>
 					<b>This project had us learn & use:</b>
-					<ul>
-						<li>HTML, JavaScript, and CSS</li>
-						<li>NodeJS & NPM</li>
-						<li>Git SCM</li>
-						<li>React (JSX, create-react-app)</li>
-						<li>SASS (SCSS)</li>
-					</ul>
 				</p>
-
+				<ul>
+					<li>HTML, JavaScript, and CSS</li>
+					<li>NodeJS & NPM</li>
+					<li>Git SCM</li>
+					<li>React (JSX, create-react-app)</li>
+					<li>SASS (SCSS)</li>
+				</ul>
 				<p>
 					This is a React project built by Ben Mullan, Cian Perrett and James Cotter during our Digital Development rotation with Ageas. 
 					<br/>
 					<br/>
 					<b>Game Rules:</b>
-					<ul>
-						<li>A word is randomly selected from a list.</li>
-						<li>The player must guess all the letters of the word to win.</li>
-						<li>The player has 11 chances, each wrong guess uses up a chance.</li>
-						<li>If the player runs out of chances before the word is completely guessed, they lose.</li>
-					</ul>
 				</p>
-
+				<ul>
+					<li>A word is randomly selected from a list.</li>
+					<li>The player must guess all the letters of the word to win.</li>
+					<li>The player has 11 chances, each wrong guess uses up a chance.</li>
+					<li>If the player runs out of chances before the word is completely guessed, they lose.</li>
+				</ul>
 				<p>
 					This rotation has allowed us to improve our skills using HTML, JavaScript, CSS and React. These have allowed us to develop streamlined Ageas webpages to remove dead code, providing us with an understanding on how an official webpage would be built and utilised within an organisation. The gradual development of these skills has allowed us to work on our final project (hangman) where we were able to utilise all of our learning to produce a webpage containing complex HTML, CSS and JavaScript.
 				</p>
